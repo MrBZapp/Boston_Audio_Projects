6e1f382c990f2900a139f8041eeb8f96d700b1f6
MrBZapp <mattbzapp@gmail.com>
2014-08-25 16:28:46 -0400

    Adding voice clock project
    adding voice clock project

---
+++ Projects/KPSVoiceClock/KPSVoiceClock.c
@@ -0,0 +1,17 @@
+/*
+ * KPSVoiceClock.c
+ *
+ * Created: 8/25/2014 9:38:53 AM
+ *  Author: Matt Zapp
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
